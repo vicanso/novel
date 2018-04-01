220f9348dee1db72596a458d66b197b9d8e584b4
vicanso <vicansocanbico@gmail.com>
2018-04-01 21:23:40 +0800

    refactor: 增加书籍类别

--- admin/src/views/book/book.js
+++ admin/src/views/book/book.js
@@ -16,7 +16,7 @@ export default {
       books: null,
       form: null,
       mode: -1,
-      categoryOptions: ['玄幻', '都市', '仙侠', '科幻', '游戏', '历史'],
+      categoryOptions: ['玄幻', '都市', '仙侠', '科幻', '游戏', '历史', '灵异'],
       statusList: [
         '所有',
         '完结',
