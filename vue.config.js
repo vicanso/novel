390baa7a4c2b428a4162e627b54e8e3504e037c0
vicanso <vicansocanbico@gmail.com>
2018-10-14 13:15:06 +0800

    refactor: update base url

--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,5 @@ module.exports = {
       }
     }
   },
-  baseUrl: process.env.NODE_ENV === "production" ? "/static/" : "/"
+  baseUrl: process.env.NODE_ENV === "production" ? "/@nv/static/" : "/"
 };
